5aeba432d49be8ca159980a8359d8fe2cf0cf38a
Ekoko Fredrick <85608259+God-Coder-XX@users.noreply.github.com>
2024-05-16 14:06:26 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.FirstName;
+  const lastName = req.body.LastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us18.api.mailchimp.com/3.0/lists/c965a3b3c8";
+  const options = {
+    method: "POST",
+    auth: "Trivia1:3d257ee8002f50bff996338de14f1370-us18",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on Port 3000");
+});
+
+// Mail Chimp Api key - 3d257ee8002f50bff996338de14f1370-us18
+
+// List Id - c965a3b3c8
